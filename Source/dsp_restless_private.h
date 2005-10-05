5e8944149914cb79f20473a8e215aaf7eb449c7a
Sebastian Pipping <sebastian@pipping.org>
2005-10-05 20:07:00 +0200

    Add code of dsp_restless 1.1.1

---
+++ Source/dsp_restless_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef DSP_RESTLESS_PRIVATE_H
+#define DSP_RESTLESS_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.1.1.7"
+#define VER_MAJOR	1
+#define VER_MINOR	1
+#define VER_RELEASE	1
+#define VER_BUILD	7
+#define COMPANY_NAME	"Hartwork Project (http://www.hartwork.org)"
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"Restless Winamp Plugin"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"Restless Winamp Plugin"
+#define PRODUCT_VERSION	""
+
+#endif /*DSP_RESTLESS_PRIVATE_H*/
---
+++ cleanup.bat
@@ -0,0 +1,5 @@
+@echo off
+del Binary\*.a
+del Binary\*.def
+del Build_Temp\*.o
+del Build_Temp\*.res
